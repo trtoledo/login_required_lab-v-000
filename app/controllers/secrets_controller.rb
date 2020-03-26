499bbbac050fd20750acf6a7b98ff2a43dc11ea6
Tomas Toledo <trtoledo1977@gmail.com>
2020-03-26 07:50:09 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,14 +1,8 @@
 class SecretsController < ApplicationController
-  
+
 
   def new
 
   end
 
-
-  private
-
-  def require_login
-    return head(:forbidden) unless session.include? :user_id
-  end
 end
